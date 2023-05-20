250e7e5bcf44e6f28850e7a4017e656ce389437c
Brandon W. Kipp <brandonwkipp@gmail.com>
2023-05-19 23:07:01 -0500

    s3 bucket fixes

--- main.tf
+++ main.tf
@@ -29,7 +29,28 @@ resource "aws_s3_bucket_website_configuration" "domain" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "domain" {
+  bucket = aws_s3_bucket.domain.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "domain" {
+  bucket = aws_s3_bucket.domain.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "domain" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.domain,
+    aws_s3_bucket_public_access_block.domain,
+  ]
+
   bucket = aws_s3_bucket.domain.id
   acl    = "public-read"
 }
